d105b9c7d710b731ebf3a2dbc3d0b898dd8f6cc2
Boris Korobeinikov <slon2k@gmail.com>
2020-02-28 13:18:55 +0600

    gulp & ts config changed

--- gulpfile.js
+++ gulpfile.js
@@ -4,4 +4,23 @@ const build = require('@microsoft/sp-build-web');
 
 build.addSuppression(`Warning - [sass] The local CSS class 'ms-Grid' is not camelCase and will not be type-safe.`);
 
+const path = require('path');
+
+build.configureWebpack.mergeConfig({
+  additionalConfiguration: (generatedConfiguration) => {
+    if(!generatedConfiguration.resolve.alias){
+      generatedConfiguration.resolve.alias = {};
+    }
+
+    // shared components
+    generatedConfiguration.resolve.alias['@components'] = path.resolve( __dirname, 'lib/shared/components/')
+
+    //root src folder
+    generatedConfiguration.resolve.alias['@src'] = path.resolve( __dirname, 'lib')
+
+    return generatedConfiguration;
+  }
+});
+
+
 build.initialize(require('gulp'));
