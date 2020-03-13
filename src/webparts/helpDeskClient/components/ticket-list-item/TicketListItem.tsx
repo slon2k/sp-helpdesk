3dfa3a96fbd78247d11b81ba3bbcb1623f938ddc
Boris Korobeinikov <slon2k@gmail.com>
2020-03-13 15:12:32 +0600

    styling list items

--- src/webparts/helpDeskClient/components/ticket-list-item/TicketListItem.tsx
+++ src/webparts/helpDeskClient/components/ticket-list-item/TicketListItem.tsx
@@ -6,24 +6,52 @@ import {
   FocusZone,
   FocusZoneDirection
 } from "office-ui-fabric-react/lib/FocusZone";
-import { Icon } from "office-ui-fabric-react/lib/Icon";
+import { Icon, IIconProps } from "office-ui-fabric-react/lib/Icon";
 import { distance } from "@src/util/DateTime";
 import styles from "./TicketListItem.module.scss";
+import { css } from "office-ui-fabric-react/lib/Utilities";
+
+const renderIcon = (status: string): React.ReactElement => {
+  if (status.toLowerCase() === "open") {
+    return (
+      <Icon
+        iconName="UserWarning"
+        className={css(styles.icon, styles.iconRed)}
+      />
+    );
+  }
+
+  if (status.toLowerCase() === "closed") {
+    return (
+      <Icon
+        iconName="ReminderPerson"
+        className={css(styles.icon, styles.iconGreen)}
+      />
+    );
+  }
+
+  return (
+    <Icon
+      iconName="Telemarketer"
+      className={css(styles.icon, styles.iconYellow)}
+    />
+  );
+};
 
 const TicketListItem: React.FC<ITicketListItemProps> = ({ ticket }) => {
   return (
     <div role="row" className={styles.TicketListItem}>
       <FocusZone direction={FocusZoneDirection.horizontal}>
         <Link to={`/ticket/${ticket.Id}`}>
-          <div>
-            <Icon iconName="CompassNW" />
-            {ticket.Status}
-          </div>
-          <div>
-            <h4>{ticket.Title}</h4>
-            <p>
-              <span>Created: {distance(ticket.Created)}</span>
-            </p>
+          <div className={styles.item}>
+            <div className={styles.status}>{renderIcon(ticket.Status)}</div>
+            <div className={styles.info}>
+              <div className={styles.title}>{ticket.Title}</div>
+              <div>
+                <span>Created: {distance(ticket.Created)}</span>{"; "}
+                <span>Modified: {distance(ticket.Modified)}</span>
+              </div>
+            </div>
           </div>
         </Link>
       </FocusZone>
